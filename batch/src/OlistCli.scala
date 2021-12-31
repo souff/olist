9b59abf4fa41199cd1b2431eb7ff9375edcd2e64
souff <leo.bonnand.germain@wanadoo.fr>
2021-12-31 16:31:27 +0100

    add arguments (in and out)

--- batch/src/OlistCli.scala
+++ batch/src/OlistCli.scala
@@ -1,11 +1,9 @@
 import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.functions.desc
+import org.apache.spark.sql.functions._
 
 object OlistCli {
-
-
-  def listDelayedDeliveries(spark: SparkSession) = {
-    val df_order = spark.read.option("header", true).csv("data/olist_orders_dataset.csv")
+  def listDelayedDeliveries(spark: SparkSession, in: String, out: String) = {
+    val df_order = spark.read.option("header", true).csv(in + "olist_orders_dataset.csv").withColumn("order_utc", to_utc_timestamp(col("order_purchase_timestamp"), "Brazil/East"))
     df_order.createOrReplaceTempView("orders")
     val delivrey_date_null = spark.sql(
       """
@@ -15,18 +13,18 @@ object OlistCli {
         FROM   (SELECT order_id,
                        customer_id,
                        order_status,
-                       To_utc_timestamp(order_purchase_timestamp, 'Brazil/East') AS order_utc
+                       order_utc
                 FROM   orders
                 WHERE  order_status NOT IN ( 'canceled', 'unavailable' ) AND order_delivered_customer_date IS NULL)
         WHERE Datediff(To_timestamp("2018-09-03 12:06:57"), order_utc) >10
         ORDER  BY delay DESC
         """)
 
-    val df_customer = spark.read.option("header", true).csv("data/olist_customers_dataset.csv")
+    val df_customer = spark.read.option("header", true).csv(in + "olist_customers_dataset.csv")
     df_customer.createOrReplaceTempView("customer")
 
 
-    val df_timezone = spark.read.option("header", true).csv("data/time_zone.csv")
+    val df_timezone = spark.read.option("header", true).csv(in + "time_zone.csv")
     df_timezone.createOrReplaceTempView("timezone")
     val delevery_with_date = spark.sql(
       """
@@ -35,7 +33,7 @@ object OlistCli {
       FROM   (SELECT o.order_id,
                      o.customer_id,
                      o.order_status,
-                     To_utc_timestamp(order_purchase_timestamp, 'Brazil/East') AS order_utc,
+                     order_utc,
                      To_utc_timestamp(o.order_delivered_customer_date, t.tz) AS delivered_utc
               FROM   customer c
                      JOIN timezone t
@@ -48,7 +46,7 @@ object OlistCli {
     """
     )
     val df_delivery = delivrey_date_null.union(delevery_with_date).sort(desc("delay"))
-    df_delivery.repartition(1).write.mode("Overwrite").option("header", "true").csv("csv")
+    df_delivery.repartition(1).write.mode("Overwrite").option("header", "true").csv(out)
   }
 
   def run(f: SparkSession => Unit) = {
@@ -59,7 +57,8 @@ object OlistCli {
   }
 
   def main(args: Array[String]) = {
+    val arguments = if (args.isEmpty) Array("data/", "csv") else args
     println("Olist Cli")
-    run(listDelayedDeliveries _)
+    run((spark: SparkSession) => listDelayedDeliveries(spark, arguments(0), arguments(1)))
   }
 }
